5b588a8ae8dc7b690ed3868a07b04e9bda41f8dc
waseyquraishi <waseyquraishi@waseys-mbp.t-mobile.com>
2023-01-15 18:18:16 -0600

    i am uploading the files

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author waseyquraishi
+ *
+ */
+module myfirsttraining {
+}
---
+++ src/myfirsttraining/hello.java
@@ -0,0 +1,15 @@
+package myfirsttraining;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("welcome to the training ");	
+System.out.println("this is afsha jabeen ");
+System.out.println("i am from chicago ");
+System.out.println("i am taking classes from codelab ");
+
+		
+	}
+
+}
