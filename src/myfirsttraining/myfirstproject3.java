24a711e06ce3860fd63feee6d784d587516d90ee
waseyquraishi <waseyquraishi@waseys-mbp.t-mobile.com>
2023-01-17 23:04:36 -0600

    Walmart super center

---
+++ src/myfirsttraining/myfirstproject3.java
@@ -0,0 +1,13 @@
+package myfirsttraining;
+
+public class myfirstproject3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to walmart");
+		System.out.println("one stop shopping experiance");
+		System.out.println("a grocery store with fresh bakery and dairy product with electronics ");
+
+	}
+
+}
