fdbeb4faa3a19d95beb16f681a78bab916f2c6b9
waseyquraishi <waseyquraishi@waseys-mbp.t-mobile.com>
2023-01-22 21:36:42 -0600

    Andrew transport

---
+++ src/myfirsttraining/mynewproject00.java
@@ -0,0 +1,13 @@
+package myfirsttraining;
+
+public class mynewproject00 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to the Andrew transport");
+		System.out.println("num of vehicles - 150");
+		System.out.println("num of employees - 50");
+
+	}
+
+}
