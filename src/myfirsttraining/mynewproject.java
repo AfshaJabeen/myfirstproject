ac7ad7d756e3a7126fb5d3af7416d2235168f365
waseyquraishi <waseyquraishi@waseys-mbp.t-mobile.com>
2023-01-22 23:19:23 -0600

    new file

---
+++ src/myfirsttraining/mynewproject.java
@@ -0,0 +1,16 @@
+package myfirsttraining;
+
+public class mynewproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("llc contruction");
+		System.out.println("our services");
+		System.out.println("kitchen renovation");
+		System.out.println("bathroom renovation");
+		System.out.println("home extention");
+		System.out.println("warm up ur home");
+
+	}
+
+}
--- src/myfirsttraining/myprojectafsha.java
+++ src/myfirsttraining/myprojectafsha.java
@@ -4,6 +4,9 @@ public class myprojectafsha {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("this is a Devops classes");
+		System.out.println("we are under the training");
+		
 
 	}
 
